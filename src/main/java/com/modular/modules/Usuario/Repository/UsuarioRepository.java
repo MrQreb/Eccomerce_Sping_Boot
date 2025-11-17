a33ada1aa25022a9435d29b4f4fd2ffabf2411c4
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2025-11-17 13:11:13 -0600

    sql lite configurado y funcionando

--- src/main/java/com/modular/modules/Usuario/Repository/UsuarioRepository.java
+++ src/main/java/com/modular/modules/Usuario/Repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.modular.modules.Usuario.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.modular.modules.Usuario.Entity.UsuarioEntity;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long>{
 
 };
